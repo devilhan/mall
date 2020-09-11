93f1ca9b572e71de321721ae1b73026eac4e7073
devilhan <hanyanjiao_hyj@163.com>
2020-09-11 10:11:08 +0800

    '初级电商'

---
+++ src/main/java/com/devil/mall/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.devil.mall.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Hanyanjiao
+ * @date 2020/8/5
+ */
+@Configuration
+@ComponentScan("com.devil")
+public class AppConfig {
+
+}
---
+++ src/main/java/com/devil/mall/controller/Test.java
@@ -0,0 +1,16 @@
+package com.devil.mall.controller;
+
+/**
+ * @author Hanyanjiao
+ * @date 2020/8/5
+ */
+public class Test {
+
+    public static void main(String[] args){
+        new Thread(){
+
+        };
+
+    }
+}
+
